d43c38e242d97bb5ccbfcdbff8fcaa1a1e903b1d
binhho1099 <binhho1099@gmail.com>
2021-08-03 22:57:34 +0700

    update bitis

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,6 +2,11 @@ const menu = document.getElementById('menu');
 const toggle = document.getElementById('toggle');
 const overlay = document.querySelector('.menu-overlay');
 const menuContent = document.querySelector('.menu-content');
+const menuItem = document.querySelectorAll('.menu-item');
+
+menuItem.forEach(i => i.addEventListener('click',()=>{
+    menu.classList.remove('menu-show');
+}))
 
 toggle.addEventListener('click',()=>{
     menu.classList.add('menu-show');
@@ -31,9 +36,48 @@ function removeLanguage(){
     languageMenu.classList.remove('language-show');
 }
 times.addEventListener('click', removeLanguage);
-
 languageOverlay.addEventListener('click',removeLanguage);
-
 languageBox.addEventListener('click',(event)=>{
     event.stopPropagation();
+});
+
+
+// back to top
+const toTop = document.querySelector('.to-top');
+
+window.addEventListener('scroll', ()=>{
+    if(window.pageYOffset > 100){
+        toTop.classList.add('to-top-active');
+    }else{
+        toTop.classList.remove('to-top-active');
+    }
 })
+
+
+// loading
+$(window).on('load', function(event){
+    $('.loading').delay(1000).fadeOut('fast');
+});
+
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
